921bc62faa82a85c405fdc614903e8fbe1b2d48c
Ivan Chichvarin <ichichvarin@humanplus.ru>
2024-05-19 23:05:27 +0300

    basic structure

---
+++ main.cpp
@@ -0,0 +1,49 @@
+/*
+ * @Author: Ivan Chichvarin ichichvarin@humanplus.ru
+ * @Date: 2024-05-19 22:51:47
+ * @LastEditors: Ivan Chichvarin ichichvarin@humanplus.ru
+ * @LastEditTime: 2024-05-19 22:55:57
+ * @FilePath: /XmlClass/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+// не меняйте файлы xml.h и xml.cpp
+#include "xml.h"
+
+using namespace std;
+
+struct Spending {
+    string category;
+    int amount;
+};
+
+int CalculateTotalSpendings(const vector<Spending>& spendings) {
+    return accumulate(spendings.begin(), spendings.end(), 0,
+                      [](int current, const Spending& spending) {
+                          return current + spending.amount;
+                      });
+}
+
+string FindMostExpensiveCategory(const vector<Spending>& spendings) {
+    assert(!spendings.empty());
+    auto compare_by_amount = [](const Spending& lhs, const Spending& rhs) {
+        return lhs.amount < rhs.amount;
+    };
+    return max_element(begin(spendings), end(spendings), compare_by_amount)->category;
+}
+
+vector<Spending> LoadFromXml(istream& input) {
+    // место для вашей реализации
+    // пример корректного XML-документа в условии
+}
+
+int main() {
+    const vector<Spending> spendings = LoadFromXml(cin);
+    cout << "Total "sv << CalculateTotalSpendings(spendings) << '\n';
+    cout << "Most expensive is "sv << FindMostExpensiveCategory(spendings) << '\n';
+}
